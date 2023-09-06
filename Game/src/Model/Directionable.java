4e2d3b7019147a359362aa8c0434b6e64eba5609
Alban999 <74149424+Alban999@users.noreply.github.com>
2023-09-06 12:34:13 -0400

    Delete Game directory

--- Game/src/Model/Directionable.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-//Gère le set et le get des directions
-public interface Directionable {
-
-	void setDirection(String _direction);
-	String getDirection();
-}
--- Game/src/Model/Life.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-//Gère le fait d'être blessé
-
-public interface Life {
-void hurt(int _value);
-}
